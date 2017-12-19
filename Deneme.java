42198aaf3814017a40b758489f3f4dce94a01f22
muhlif <34690292+muhlif@users.noreply.github.com>
2017-12-19 23:01:36 +0300

    Add files via upload

---
+++ Deneme.java
@@ -0,0 +1,18 @@
+
+package deneme;
+
+import static java.time.Clock.system;
+
+
+public class Deneme {
+
+    
+     
+     
+    public static void main(String[] args) {
+       
+        
+         System.out.println("MERHABA ARKADAŞIM");
+    }
+    
+}
